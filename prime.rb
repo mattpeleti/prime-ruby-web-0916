5a66694db32ad922b6aea4b5232ade5cf7c29681
Matt Peleti <mattpeleti@gmail.com>
2016-09-25 16:01:37 +0000

    what's a prime number?

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(num)
+  return false if num <= 1
+  return true if num == 2
+  counter = 2
+  until(counter ** 2 > num + 3)
+    if(num % counter == 0)
+      return false
+    else
+      counter += 1
+    end
+ end
+ return true
+end
